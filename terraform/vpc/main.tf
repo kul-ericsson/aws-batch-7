5865fc81fc0d15bb48fcd3a5693447befb3ebd31
Kulbhushan <kulbhushan.mayer@>
2021-01-16 11:25:00 +0530

    Added Subnet Resource to Create Public Subnet

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -34,3 +34,13 @@ resource "aws_route_table" "igw_rt" {
       gateway_id = aws_internet_gateway.igw.id
   }
 }
+
+resource "aws_subnet" "sn-public" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.10.10.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "${var.tagName}_public"
+  }
+}
