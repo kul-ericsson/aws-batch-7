b23dc9e364dc3d12aba279eda6cc057d1c7ee066
Kulbhushan <kulbhushan.mayer@>
2021-01-16 12:27:28 +0530

    Added NAT Gateway Resource

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -75,3 +75,11 @@ resource "aws_eip" "eip" {
   }
 }
 
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id = aws_subnet.sn_public.id
+  tags = {
+    "Name" = var.tagName
+  }
+}
+
