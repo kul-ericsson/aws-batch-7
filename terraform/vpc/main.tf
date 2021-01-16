1a55965c5b804be62276a0c26cf85de2468faf7b
Kulbhushan <kulbhushan.mayer@>
2021-01-16 10:44:44 +0530

    Added VPC Resource

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.10.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+      Name = "Kul"
+  }
+}
