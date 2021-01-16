2dd8835d30b7914f6fd4c1ce5300c62de90bc462
Kulbhushan <kulbhushan.mayer@>
2021-01-16 13:39:08 +0530

    Added Resource to created Security Group

--- terraform/createCompute/main.tf
+++ terraform/createCompute/main.tf
@@ -12,3 +12,26 @@ resource "aws_instance" "ec2" {
     "Name" = var.tagName
   }
 }
+
+resource "aws_security_group" "sg" {
+  vpc_id = data.aws_vpc.vpc.id
+  description = "Managed by Terraform"
+  name = "${var.tagName}"
+  tags = {
+    "Name" = var.tagName
+  }
+  ingress {
+    description = "SSH Port"
+    protocol = "tcp"
+    from_port = 22
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Outbound Access"
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
