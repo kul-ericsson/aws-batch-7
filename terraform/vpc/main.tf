63965fbf7231d46aa30caad73e78b5d8e587aed0
Kulbhushan <kulbhushan.mayer@>
2021-01-16 10:53:59 +0530

    Added Variables to be used

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -2,19 +2,24 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "tagName" {
+  Description = "Type your Name to be used as TAGNAME for resources"
+  type = String
+}
+
 resource "aws_vpc" "vpc" {
   cidr_block = "10.10.0.0/16"
   instance_tenancy = "default"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
-      Name = "Kul"
+      Name = var.tagName
   }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.vpc_id
   tags = {
-    "Name" = "Kul"
+    "Name" = var.tagName
   }
 }
